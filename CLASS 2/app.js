edd8186c25f0a02566360fef4583da958d0e0135
jawairiafatima <jawairiafatima24@gmail.com>
2025-12-27 11:39:00 +0500

    first commit

---
+++ CLASS 2/app.js
@@ -0,0 +1,36 @@
+var firstName = "jawairia"
+console.log("firstName")
+
+firstName = "jawairia \n fatima"
+console.log(firstName);
+
+// 1. Declare a variable called username
+var username;
+
+// 2. Declare myName and assign full name
+var myName = "Jawairia Fatima";
+    .console.log(myName);
+
+
+// 3. Message variable
+var message;
+message = "Hello World";
+    .console.log(message);
+
+
+// 4. Student bio data
+var studentName = "jawairia fatima";
+    .console.log(studentName);
+
+var studentAge = "16 years old";
+    .console.log(studentAge);
+
+var studentCourse = "Web Development";
+    .console.log(studentCourse);
+
+
+var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+     .console.log(message);
+
+var email = "example@example.com";
+.console.log(email);
